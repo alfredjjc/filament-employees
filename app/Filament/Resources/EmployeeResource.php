6757eafca4652306c0454567d0cc86107020284a
AlfredBypassr <alfredcheong@bypassr.com>
2022-11-21 16:30:50 +0800

    widget update

--- app/Filament/Resources/EmployeeResource.php
+++ app/Filament/Resources/EmployeeResource.php
@@ -35,44 +35,44 @@ class EmployeeResource extends Resource
         return $form
             ->schema([
                 Card::make()
-                ->schema([
-                    Select::make('country_id')
-                    ->label('Country')
-                    ->options(Country::all()->pluck('name', 'id')->toArray())
-                    ->required()
-                    ->reactive()
-                    ->afterStateUpdated(fn (callable $set) => $set('state_id', null)),
-                Select::make('state_id')
-                    ->label('State')
-                    ->required()
-                    ->options(function (callable $get) {
-                        $country = Country::find($get('country_id'));
-                        if (!$country) {
-                            return State::all()->pluck('name', 'id');
-                        }
-                        return $country->states->pluck('name', 'id');
-                    })
-                    ->reactive()
-                    ->afterStateUpdated(fn (callable $set) => $set('city_id', null)),
-                Select::make('city_id')
-                    ->label('City')
-                    ->options(function (callable $get) {
-                        $state = State::find($get('state_id'));
-                        if (!$state) {
-                            return City::all()->pluck('name', 'id');
-                        }
-                        return $state->cities->pluck('name', 'id');
-                    })
-                    ->required()
-                    ->reactive(),
-                Select::make('department_id')
-                    ->relationship('department', 'name')->required(),
-                TextInput::make('first_name')->required()->maxLength(255),
-                TextInput::make('last_name')->required()->maxLength(255),
-                TextInput::make('address')->required()->maxLength(255),
-                TextInput::make('zip_code')->required()->maxLength(7),
-                DatePicker::make('birth_date')->required(),
-                DatePicker::make('date_hired')->required(), 
+                    ->schema([
+                        Select::make('country_id')
+                            ->label('Country')
+                            ->options(Country::all()->pluck('name', 'id')->toArray())
+                            ->required()
+                            ->reactive()
+                            ->afterStateUpdated(fn (callable $set) => $set('state_id', null)),
+                        Select::make('state_id')
+                            ->label('State')
+                            ->required()
+                            ->options(function (callable $get) {
+                                $country = Country::find($get('country_id'));
+                                if (!$country) {
+                                    return State::all()->pluck('name', 'id');
+                                }
+                                return $country->states->pluck('name', 'id');
+                            })
+                            ->reactive()
+                            ->afterStateUpdated(fn (callable $set) => $set('city_id', null)),
+                        Select::make('city_id')
+                            ->label('City')
+                            ->options(function (callable $get) {
+                                $state = State::find($get('state_id'));
+                                if (!$state) {
+                                    return City::all()->pluck('name', 'id');
+                                }
+                                return $state->cities->pluck('name', 'id');
+                            })
+                            ->required()
+                            ->reactive(),
+                        Select::make('department_id')
+                            ->relationship('department', 'name')->required(),
+                        TextInput::make('first_name')->required()->maxLength(255),
+                        TextInput::make('last_name')->required()->maxLength(255),
+                        TextInput::make('address')->required()->maxLength(255),
+                        TextInput::make('zip_code')->required()->maxLength(7),
+                        DatePicker::make('birth_date')->required(),
+                        DatePicker::make('date_hired')->required(),
                     ])
             ]);
     }
@@ -98,14 +98,21 @@ class EmployeeResource extends Resource
                 Tables\Actions\DeleteBulkAction::make(),
             ]);
     }
-    
+
     public static function getRelations(): array
     {
         return [
             //
         ];
     }
-    
+
+    public static function getWidgets(): array
+    {
+        return [
+            EmployeeStatsOverview::class,
+        ];
+    }
+
     public static function getPages(): array
     {
         return [
@@ -113,5 +120,5 @@ class EmployeeResource extends Resource
             'create' => Pages\CreateEmployee::route('/create'),
             'edit' => Pages\EditEmployee::route('/{record}/edit'),
         ];
-    }    
+    }
 }
